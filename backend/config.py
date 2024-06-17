e7ac3c3a525300920750e99fe19e0960b5383149
ahmedtiko <ahmedmm_tarek@hotmail.com>
2024-06-17 14:15:39 +0300

    backend phase 1

---
+++ backend/config.py
@@ -0,0 +1,7 @@
+import os
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'your_secret_key'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'mysql+pymysql://username:password@localhost/biddify'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
