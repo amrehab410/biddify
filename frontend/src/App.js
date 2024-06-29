084dcfb63ef4fe68294720f8efe3ee0717ec435a
AhmedHesham2112 <AhmedHesham2112>
2024-06-29 16:40:06 +0300

    nav-bar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,11 +9,13 @@ import PrivateRoute from './components/PrivateRoute';
 import CreateAuction from './components/CreateAuction';
 import './App.css'
 import AuctionsPage from './components/AuctionsPage';
+import Navbar from "./components/Navbar";
 
 function App() {
     return (
         <AuthProvider>
             <Router>
+            <Navbar />
                 <Routes>
                     <Route path="/login" element={<Login />} />
                     <Route path="/register" element={<Register />} />
