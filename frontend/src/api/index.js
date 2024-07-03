51d07fa9f186ea97c1065993fa112952da265e55
amrehab410 <amrehab410@gmail.com>
2024-07-03 22:48:29 +0300

    build

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://localhost:9000';
+const API_URL = 'https://amrehab410.pythonanywhere.com/';
 
 const request = async (url, method, body = null) => {
     const headers = { 'Content-Type': 'application/json' };
